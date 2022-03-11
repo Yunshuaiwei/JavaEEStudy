2561c11397cf5b72ef976ba7b7f0744e2713210b
yunshuaiwei <1779001867@qq.com>
2022-03-11 10:38:06 +0800

    add：练习

--- springboot02/src/main/java/ysw/RedisTest.java
+++ springboot02/src/main/java/ysw/RedisTest.java
@@ -1,8 +1,10 @@
 package ysw;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,27 +15,36 @@ import java.util.concurrent.TimeUnit;
  */
 public class RedisTest {
 
+    @Autowired
     private RedisTemplate redisTemplate;
 
-
-    public void testLock(){
-
+    /**
+     * @author yunshuaiwei
+     * @description 分布式锁测试
+     * @date 10:24 2022/3/11
+     * @Param []
+     * @Return void
+     */
+    public void testLock() {
+        String uuid = UUID.randomUUID().toString();
         //获取锁
-        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", "11",5, TimeUnit.SECONDS);
-
-        if (lock){//获取锁成功
+        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", uuid, 5, TimeUnit.SECONDS);
+        if (lock) {//获取锁成功
             //获取值
             Object value = redisTemplate.opsForValue().get("num");
-            if (StringUtils.isEmpty(value)){
+            if (StringUtils.isEmpty(value)) {
                 return;
             }
             //将值转为int
             int num = Integer.parseInt(value + "");
             //操作数据
-            redisTemplate.opsForValue().set("num",++num);
+            redisTemplate.opsForValue().set("num", ++num);
             //释放锁
-            redisTemplate.delete("lock");
-        }else{//获取锁失败
+            String lockUUID = (String) redisTemplate.opsForValue().get("lock");
+            if (lockUUID.equals(uuid)) {
+                redisTemplate.delete("lock");
+            }
+        } else {//获取锁失败
             try {
                 //间隔0.1s重新获取锁
                 Thread.sleep(100);
